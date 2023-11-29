37ba2f3ebf53627de1f8a438a15b4b09cf3933ac
David <d0pokrajac@gmail.com>
2023-11-29 09:02:49 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  httpClient = inject(HttpClient);
+  dataList:any[] = [];
+
+  ngOnInit(): void {
+    this.fetchData();
+  } 
+  
+  fetchData() {
+    this.httpClient.get('https://jsonplaceholder.typicode.com/posts')
+    .subscribe((data: any) => {
+      console.log(data);
+      this.dataList = data;
+    })
+  }
 }
